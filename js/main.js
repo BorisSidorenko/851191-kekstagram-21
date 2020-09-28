ed6275ed33deb93bc17bdb193e62dc233b79e23f
Boris <boris.m.sidorenko@gmail.com>
2020-09-28 21:21:55 +0300

    Добавил отображение первой фотографии с комментариями

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const Photo = {
   LIKES_COUNT_MIN: 15,
   LIKES_COUNT_MAX: 200,
   COMMENTS_COUNT_MIN: 0,
-  COMMENTS_COUNT_MAX: 6,
+  COMMENTS_COUNT_MAX: 10,
   URL_TEMPLATE: 'photos/.jpg',
   IMG_COUNT_MAX: 25,
   IMG_COUNT_MIN: 1
@@ -38,8 +38,10 @@ const Commentator = {
   AVATAR_NUMBER_MAX: 6
 };
 
+const body = document.body;
 const pictureTemplate = document.querySelector('#picture').content;
 const picturesContainer = document.querySelector('.pictures');
+const bigPicture = document.querySelector('.big-picture');
 
 const getRandomArrValue = (arrLength) => Math.floor(Math.random() * arrLength);
 
@@ -50,7 +52,7 @@ const getPhotoPath = (number) => `photos/${number}.jpg`;
 const getAvatarPath = (number) => `img/avatar-${number}.svg`;
 
 const getCommentator = () => ({
-  avatar: getAvatarPath(Commentator.AVATAR_NUMBER_MAX, Commentator.AVATAR_NUMBER_MIN),
+  avatar: getAvatarPath(getRandomNumberMaxToMin(Commentator.AVATAR_NUMBER_MAX, Commentator.AVATAR_NUMBER_MIN)),
   message: Commentator.MESSAGES[getRandomArrValue(Commentator.MESSAGES.length)],
   name: Commentator.NAMES[getRandomArrValue(Commentator.NAMES.length)]
 });
@@ -58,13 +60,13 @@ const getCommentator = () => ({
 const getCommentators = (count) => new Array(count).fill(undefined).map(getCommentator);
 
 const getPhoto = (index) => ({
-  url: getPhotoPath(index),
+  url: getPhotoPath(++index),
   description: '',
   likes: getRandomNumberMaxToMin(Photo.LIKES_COUNT_MAX, Photo.LIKES_COUNT_MIN),
   comments: getCommentators(getRandomNumberMaxToMin(Photo.COMMENTS_COUNT_MAX))
 });
 
-const getPhotos = (count) => new Array(count).fill(undefined).map((element, index) => getPhoto(++index));
+const getPhotos = (count) => new Array(count).fill(undefined).map((element, index) => getPhoto(index));
 
 const addPhotoToFragment = (fragment) => (photo) => fragment.appendChild(renderPicture(photo));
 
@@ -86,4 +88,53 @@ const renderPicture = (photo) => {
   return pictureElement;
 };
 
+const hideCommentsCounter = () => document.querySelector('.social__comment-count').classList.add('hidden');
+
+const hideCommentsLoader = () => document.querySelector('.comments-loader').classList.add('hidden');
+
+const renderComments = (arrComments) => {
+  const commentsFragment = document.createDocumentFragment();
+
+  arrComments.forEach((commentator) => addCommentToFragment(commentsFragment, commentator));
+
+  removeDefaultComments();
+
+  return commentsFragment;
+};
+
+const addCommentToFragment = (fragment, commentator) => {
+  const commentElement = bigPicture.querySelector('.social__comment').cloneNode(true);
+  commentElement.querySelector('img').src = commentator.avatar;
+  commentElement.querySelector('img').alt = commentator.name;
+  commentElement.querySelector('.social__text').textContent = commentator.message;
+  fragment.appendChild(commentElement);
+};
+
+const removeDefaultComments = () => {
+  Array.from(bigPicture.querySelectorAll('.social__comment')).forEach((element) => element.remove());
+};
+
+const rendenderPhotoAndComments = (photo) => {
+  bigPicture.querySelector('.big-picture__img').querySelector('img').src = photo.url;
+  bigPicture.querySelector('.likes-count').textContent = photo.likes;
+  bigPicture.querySelector('.comments-count').textContent = photo.comments.length;
+  bigPicture.querySelector('.social__comments').appendChild(renderComments(photo.comments));
+  bigPicture.querySelector('.social__caption').textContent = photo.description;
+};
+
+const showBigPicture = (photo) => {
+  rendenderPhotoAndComments(photo);
+
+  hideCommentsCounter();
+  hideCommentsLoader();
+
+  body.classList.add('modal-open');
+  bigPicture.classList.remove('hidden');
+};
+
+
 picturesContainer.appendChild(createPictureFragment());
+
+showBigPicture(getPhoto(0));
+
+

43dded23115c7e41d5e422d2646784616194f90a
Boris <boris.m.sidorenko@gmail.com>
2020-10-21 15:55:08 +0300

    Поправил замечания

--- js/filter.js
+++ js/filter.js
@@ -1,22 +1,21 @@
 'use strict';
 
 (() => {
-  const FilterIds = {
-    DEFAULT: "filter-default",
-    RANDOM: "filter-random",
-    DISCUSSED: "filter-discussed"
-  };
-
   const RANDOM_PHOTOS_AMOUNT = 10;
+  const ACTIVE_FILTER_CLASS = 'img-filters__button--active';
 
   const picturesContainer = document.querySelector('.pictures');
   const filterForm = document.querySelector('.img-filters__form');
 
-  const ACTIVE_FILTER_CLASS = 'img-filters__button--active';
-
   const sortByDiscussed = (photos) => photos.sort((a, b) => b.comments.length - a.comments.length);
 
   const sortInRandomOrder = (photos) => shufflePhotos(photos).splice(RANDOM_PHOTOS_AMOUNT);
+
+  const filterMap = new Map([
+    [`filter-random`, sortInRandomOrder],
+    [`filter-discussed`, sortByDiscussed],
+    [`filter-default`, () => {}]
+  ]);
 
   const filter = {
     onFilterChange: () => {}
@@ -34,6 +33,7 @@
 
   const clearPicturesContainer = () => {
     const children = Array.from(picturesContainer.children);
+
     children.forEach((child) => {
       if (child.classList.contains('picture')) {
         child.remove();
@@ -43,27 +43,20 @@
 
   const changeActiveFilter = (evt) => {
     const activeFilter = filterForm.querySelector(`.${ACTIVE_FILTER_CLASS}`);
+
     if (activeFilter) {
       activeFilter.classList.remove(ACTIVE_FILTER_CLASS);
     }
+
     evt.target.classList.add(ACTIVE_FILTER_CLASS);
   };
 
   const applyFilter = (evt, photos) => {
     let photosToSort = Array.from(photos);
 
-    clearPicturesContainer();
+    filterMap.get(evt.target.id)(photosToSort);
 
-    switch (evt.target.id) {
-      case FilterIds.RANDOM:
-        sortInRandomOrder(photosToSort);
-        break;
-      case FilterIds.DISCUSSED:
-        sortByDiscussed(photosToSort);
-        break;
-      default:
-        break;
-    }
+    clearPicturesContainer();
     window.data.renderPhotos(photosToSort);
   };
 

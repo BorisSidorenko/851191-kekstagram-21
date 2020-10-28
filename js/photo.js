79fca5aea360e502dbf734cbf4e1eaaae887b5df
Boris <boris.m.sidorenko@gmail.com>
2020-10-28 17:55:17 +0300

    Добавил выбор фотографии для редактирования

---
+++ js/photo.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const inputUpload = document.querySelector('#upload-file');
+const previewContainer = document.querySelector('.img-upload__preview');
+const preview = previewContainer.querySelector('img');
+
+const acceptedTypes = inputUpload.accept.split(',');
+const previewDefaultSrc = preview.src;
+
+const onFileLoad = (evt) => {
+  preview.src = evt.target.result;
+};
+
+const onInputUploadChange = () => {
+  const file = inputUpload.files[0];
+
+  if (acceptedTypes.some((type) => type.trim() === file.type)) {
+    const reader = new FileReader();
+
+    reader.addEventListener('load', onFileLoad);
+
+    reader.readAsDataURL(file);
+  } else {
+    preview.src = previewDefaultSrc;
+  }
+};
+
+inputUpload.addEventListener('change', onInputUploadChange);
